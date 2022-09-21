3437bfa53b8594c64c43ac5b304d36c4ec0aadfb
tanzi <tanzi@DESKTOP-H8U1H3U.fios-router.home>
2022-09-21 11:17:00 -0400

    commit

--- Demo_Selenium/src/selenium_package/practice.java
+++ Demo_Selenium/src/selenium_package/practice.java
@@ -3,14 +3,16 @@ package selenium_package;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Practice {
+public class practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver.exe");
-		WebDriver x = new ChromeDriver();
-		x.get("http://training.qaonlinetraining.com/testPage.php");	
+		  System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver.exe");
+		  WebDriver x = new ChromeDriver();
+			x.get("https://www.google.com/");	
+		
+		
 		
 		
 		
